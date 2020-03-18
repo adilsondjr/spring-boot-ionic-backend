6ea023642ca7ee4f314e97fd32a0ceb332a6999e
Adilson Domiciano <adilsondjr@gmail.com>
2020-03-18 16:53:15 -0300

    create page on Categorias with opational params into request

--- src/main/java/br/com/adilsondjr/cursomc/CursomcApplication.java
+++ src/main/java/br/com/adilsondjr/cursomc/CursomcApplication.java
@@ -60,8 +60,16 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Categoria cat1 = new Categoria(null, "Informatica");
-		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat1 = new Categoria(null, "Cama mesa e banho");
+		Categoria cat2 = new Categoria(null, "Eletronicos");
+		Categoria cat3 = new Categoria(null, "Jogos");
+		Categoria cat4 = new Categoria(null, "Jardinagem");
+		Categoria cat5 = new Categoria(null, "Decoração");
+		Categoria cat6 = new Categoria(null, "Perfumaria");
+		Categoria cat7 = new Categoria(null, "Utencilios e Acessórios");
+		Categoria cat8 = new Categoria(null, "Higiene");
+		Categoria cat9 = new Categoria(null, "Bebidas");
+		Categoria cat10 = new Categoria(null, "Enxoval");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -113,7 +121,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getItens().addAll(Arrays.asList(ip3));
 		p2.getItens().addAll(Arrays.asList(ip2));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));

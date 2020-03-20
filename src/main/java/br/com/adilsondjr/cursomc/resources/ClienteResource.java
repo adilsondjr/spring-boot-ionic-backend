4832b9e5f7e47a6ca952728df8a42f6df74786f5
Adilson Domiciano <adilsondjr@gmail.com>
2020-03-19 21:47:18 -0300

    Create PUT, DELETE and GET to Cliente

--- src/main/java/br/com/adilsondjr/cursomc/resources/ClienteResource.java
+++ src/main/java/br/com/adilsondjr/cursomc/resources/ClienteResource.java
@@ -1,18 +1,31 @@
 package br.com.adilsondjr.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.adilsondjr.cursomc.domain.Cliente;
+import br.com.adilsondjr.cursomc.dto.ClienteDTO;
 import br.com.adilsondjr.cursomc.services.ClienteService;
 
 @RestController
 @RequestMapping(path = "/cliente")
 public class ClienteResource {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(ClienteResource.class);
 	
 	@Autowired
 	private ClienteService service; 
@@ -22,6 +35,53 @@ public class ClienteResource {
 		
 		Cliente c1 = service.find(id);		
 		return ResponseEntity.ok().body(c1);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		
+		LOG.info("Buscando todas as Clientes: ");
+		List<Cliente> cli = service.findAll();
+		List<ClienteDTO> cliDTO = cli.stream().map(clis -> new ClienteDTO(clis)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(cliDTO);
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		
+		LOG.info("Buscando todas as Clientes: ");
+		Page<Cliente> cli = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> cliDTO = cli.map(clis -> new ClienteDTO(clis));
+		return ResponseEntity.ok().body(cliDTO);
+	}
+	
+	/*@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO cliDTO){
+		Cliente cli = service.fromDTO(cliDTO);
+		cli = service.insert(cli);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(cli.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}*/
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO cliDTO, @PathVariable Integer id){
+		Cliente cli = service.fromDTO(cliDTO);
+		cli.setId(id);
+		cli = service.update(cli);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/br/com/adilsondjr/cursomc/services/ClienteService.java
+++ src/main/java/br/com/adilsondjr/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.adilsondjr.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.adilsondjr.cursomc.domain.Cliente;
+import br.com.adilsondjr.cursomc.dto.ClienteDTO;
 import br.com.adilsondjr.cursomc.repositories.ClienteRepository;
+import br.com.adilsondjr.cursomc.services.exceptions.DataIntegrityException;
 import br.com.adilsondjr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,6 +28,41 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Cliente update(Cliente cli) {
+		Cliente cliente = find(cli.getId());
+		updateData(cliente, cli);
+		return repo.save(cliente);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
+	
+	public Cliente fromDTO(ClienteDTO cliDTO) {
+		return new Cliente(cliDTO.getId(), cliDTO.getNome(), cliDTO.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente cliente, Cliente cli) {
+		cliente.setNome(cli.getNome());
+		cliente.setEmail(cli.getEmail());
 	}
 
 }
